53ed756d1c89d06e4c97927af1f8762ca6399d39
Justin Richter <jrichter@jetfive.com>
2014-03-27 20:13:14 -0500

    add signup and contact pages

--- app.rb
+++ app.rb
@@ -15,4 +15,23 @@ class App < Sinatra::Base
     @lineFour = '<span class="title">A</span>ssociation'
     haml :index
   end
+
+  get '/signup' do
+    @site_title = 'OSPA'
+    @lineOne = '<span class="title">O</span>klahoma'
+    @lineTwo = '<span class="title">S</span>od'
+    @lineThree = '<span class="title">P</span>roducers'
+    @lineFour = '<span class="title">A</span>ssociation'
+    haml :signup
+  end
+
+  get '/contact' do
+    @site_title = 'OSPA'
+    @lineOne = '<span class="title">O</span>klahoma'
+    @lineTwo = '<span class="title">S</span>od'
+    @lineThree = '<span class="title">P</span>roducers'
+    @lineFour = '<span class="title">A</span>ssociation'
+    haml :contact
+  end
+
 end
